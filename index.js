059f3f0250848d421a9db224b88a070a43806208
Kelley Chaplain <kmchaplain@gmail.com>
2019-12-23 19:45:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string) {
+    const nonWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let newString = string.split(" ").map(function(word) {
+      if(nonWords.find(w => w === word) === undefined)
+        {return Formatter.capitalize(word)} else {
+          return word
+        }
+    }).join(' ')
+    return Formatter.capitalize(newString)
+  }  
+
 }
