53b482e9f5179f8dcb707782a8744738c792c98c
Jess Rezac <jess.rezac@gmail.com>
2019-12-27 10:54:35 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+  /* class StaticMethod {
+	static methodName() {
+		console.log('My method is static!');
+  } */
+
+  static capitalize(str) {
+    // capitalizes first letter
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    // removes all non-alphanumeric characters except for dashes, single quotes, and spaces
+    return str.replace(/[^a-zA-Z\-'\s]/g, '')
+  }
+
+  static titleize(str) {
+    // capitalize all words except the, a, an, but, of, and, for, at, by, from
+    // always capitalize the first word
+    let splitString = str.split(' ')
+    let excludeWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    return splitString.map(word => {
+      if (splitString[0] === word) {
+        return this.capitalize(word)
+      } else if (excludeWords.includes(word)) {
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    }).join(" ")
+  }
 }
+
