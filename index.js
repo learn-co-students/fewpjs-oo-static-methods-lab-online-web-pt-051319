a8037605b531654870b658b7a1d1152a5e65591c
nbert100 <nbert14@gmail.com>
2020-01-04 12:12:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+    let sentenceArray = string.split(" ");
+    let nope = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    return sentenceArray.map(word => {
+      if (sentenceArray[0] === word) {
+        return this.capitalize(word)
+      } else if (nope.includes(word)) {
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    }).join(" ")
+    }
+    
+  }
