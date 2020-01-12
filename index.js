e0c912c8971e1bfad2b8bc443a10c16776762fbc
jasmine1226 <jasmine1226@gmail.com>
2020-01-12 00:34:19 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.substring(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z '-]/g, "");
+  }
+
+  static titleize(string) {
+    let exceptions = [
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+      "the"
+    ];
+    let words = string.split(" ").map(function(word, i) {
+      if (i != 0 && exceptions.find(a => a === word)) {
+        return Formatter.sanitize(word);
+      } else {
+        return Formatter.capitalize(Formatter.sanitize(word));
+      }
+    });
+    return words.join(" ");
+  }
 }
